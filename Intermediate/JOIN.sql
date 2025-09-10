572848fc8c9d358001d5067b6cf264acd964dd2b
SabinaS77 <36677696+SabinaS77@users.noreply.github.com>
2025-09-10 16:26:19 +0200

    Create JOIN.sql

---
+++ Intermediate/JOIN.sql
@@ -0,0 +1,50 @@
+-- Joins
+
+select *
+from employee_demographics;
+
+select *
+from employee_salary;
+
+select dem.employee_id, age, occupation
+from employee_demographics as dem
+inner join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+;
+
+
+-- Outer Joins
+
+select *
+from employee_demographics as dem
+right join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+;
+
+
+-- Self Join
+
+select emp1.employee_id as emp_santa, 
+emp1.first_name as first_name_santa,
+emp1.last_name as last_name_santa,
+emp2.employee_id as emp_emp,
+emp2.first_name as first_name_emp,
+emp2.last_name as last_name_emp
+from employee_salary emp1
+join employee_salary emp2
+	on emp1.employee_id + 1 = emp2.employee_id
+;
+
+
+-- Joining multiple tables togather
+
+select *
+from employee_demographics as dem
+inner join employee_salary as sal
+	on dem.employee_id = sal.employee_id
+inner join parks_departments pd
+	on sal.dept_id = pd.department_id
+;
+
+select *
+from parks_departments;
